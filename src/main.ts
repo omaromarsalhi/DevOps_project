3d5677e9caa545bce903ec83ce423f04de03a69a
omaromarsalhi <omar.salhi.job@gmail.com>
2025-07-14 23:27:18 +0100

    rebuilding then deploying

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:8080',
     credentials: true,
   });
 
